60765d551f71e7ccd5ac73d331b5c5f612afecc3
Alvaro Denis Acosta Quesada <denisacostaq@gmail.com>
2013-06-15 22:17:50 -0400

    first commit

---
+++ src/Source/main.c
@@ -0,0 +1,15 @@
+/*!
+  \file main.c You need edit this file and create some others for your
+  personal project.
+  \author Alvaro Denis Acosta Quesada denisacostaq\@gmail.com
+  \date Sat Jun 15 09:21:26 CDT 2013
+  
+  \brief If you are a professional emploee, is good edit this comments ;)
+
+  \attention <h1><center>&copy; COPYRIGHT GNU GENERAL PUBLIC LICENSE Version 2, June 1991</center></h1>
+*/
+int main(void )
+{
+	while (1);
+	return 0;
+}
