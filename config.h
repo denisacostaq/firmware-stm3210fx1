e13971e8cf551c6368d02466f6862be17c3e4686
Alvaro Denis Acosta Quesada <denisacostaq@gmail.com>
2013-06-16 03:04:01 -0400

    eliminados los paketes de git, o sea gitignore += tar.gz tar.bz2

---
+++ config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "firmware-stm3210fx1"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "denisacostaq@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "firmware-stm3210fx1"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "firmware-stm3210fx1 0.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "firmware-stm3210fx1"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0.0"
+
+/* Version number of package */
+#define VERSION "0.0.0"
