5fec943925179c107ad7f3e18d42d7a7ea63667e
Alvaro <denisacostaq@gmail.com>
2014-06-04 19:56:50 -0400

    Acutalizado a automake 1.14.1 y correccion de errores

--- README.md
+++
@@ -1,4 +0,0 @@
-firmware-stm3210fx1
-===================
-
-Esta plantilla es la más básica de todas, solo te garantizo que compila pero el resto deberás agregrlo tu.
--- config.h
+++ config.h
@@ -1,26 +1,23 @@
 /* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define to 1 if your C compiler doesn't accept -c and -o together. */
-/* #undef NO_MINUS_C_MINUS_O */
-
 /* Name of package */
-#define PACKAGE "firmware-stm3210fx1"
+#define PACKAGE "tstm3210fx1"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "denisacostaq@gmail.com"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "firmware-stm3210fx1"
+#define PACKAGE_NAME "template-stm3210fx1"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "firmware-stm3210fx1 0.0.0"
+#define PACKAGE_STRING "template-stm3210fx1 0.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "firmware-stm3210fx1"
+#define PACKAGE_TARNAME "tstm3210fx1"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL ""
+#define PACKAGE_URL "https://github.com/denisacostaq/template-stm3210fx1.git"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.0.0"
